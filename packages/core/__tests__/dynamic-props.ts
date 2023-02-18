3d2129ba056fb6ab9c4d119fb531fbc20933f7f1
jelee <1245183029@qq.com>
2023-02-18 22:52:07 +0800

    test(core): add test case of dynamic value

---
+++ packages/core/__tests__/dynamic-props.ts
@@ -0,0 +1,18 @@
+import { Worktable, Column } from '../src'
+
+describe('dynamic props', () => {
+  test('value', async () => {
+    const columns: Column[] = [
+      { field: 'price1', type: 'number' },
+      { field: 'price2', type: 'number' },
+      { field: 'total', value: (row) => +row.price1 + +row.price2 },
+    ]
+    const wt = new Worktable({ columns, initialData: [{ price1: 10, price2: 9 }] })
+    let data = wt.getData()
+    expect(data[0].total).toBe(19)
+
+    wt.inputValue({ rid: wt.rows[0].rid, field: 'price1' }, 20)
+    data = wt.getData()
+    expect(data[0].total).toBe(29)
+  })
+})
